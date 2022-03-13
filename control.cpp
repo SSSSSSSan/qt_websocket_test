4b1513063024e2d8968e011734df584c4a60ff51
SSSSSSSan <sanmeber@163.com>
2022-03-13 21:53:08 +0800

    control
    Signed-off-by: SSSSSSSan <sanmeber@163.com>

--- control.cpp
+++ control.cpp
@@ -2,6 +2,16 @@
 #include <QDebug>
 #include <QUrl>
 #include <QUrlQuery>
+//class userInfo
+
+void userInfo::receivedtext(QString address,quint16 ip ,QString msg)
+{
+
+}
+void userInfo::receivedData(QString address,quint16 ip ,QByteArray msg)
+{
+
+}
 control::control()
 {
 
@@ -15,14 +25,34 @@ bool control::init()
     wss * mywss = new wss();
     bool rebool=mywss->init(12345);
     connect(mywss,&wss::signal_newConnection
-            ,this,&control::slots_newU);
+            ,this,&control::slot_newU);
     return rebool;
 }
-void control::slots_newU(QString address,quint16 ip,QUrl url)
+void control::slot_newU(QString address,quint16 ip,QUrl url)
 {
     qDebug()<<address<<ip<<url;
     QUrlQuery query;
     query.setQuery( url.query());
-    qDebug()<<query.queryItemValue("id")<<query.queryItemValue("password");
+    //qDebug()<<query.queryItemValue("id")<<query.queryItemValue("password");
+    userInfo * puserInfo = new userInfo;
+    puserInfo->userAddress=address;
+    puserInfo->ip=ip;
+    _hashUserInfo.insert(query.queryItemValue("id"),puserInfo);
+}
+void control::slot_connectTo(QString id,QString toId)
+{
+    if(!_hashUserInfo.contains(id)|!_hashUserInfo.contains(toId))
+    {
+        qDebug()<<"未找到某个ID";
+    }else{
+        //_hashUserInfo.value(id)->connectToId.
+    }
+}
+void control::slot_receivedText(QString address,quint16 ip,QString msg)
+{
+
+}
+void control::slot_receivedData(QString address,quint16 ip,QByteArray msg)
+{
 
 }
--- control.h
+++ control.h
@@ -3,6 +3,20 @@
 
 #include <QObject>
 #include "wss.h"
+class userInfo : public QObject
+{
+    Q_OBJECT
+public:
+    QString userAddress = 0;
+    quint16 ip = 0;
+    QStringList teams;
+signals:
+    void sendText(QString address,quint16 ip,QString msg);
+    void sendData(QString address,quint16 ip,QByteArray msg);
+public slots:
+    void receivedtext(QString address,quint16 ip,QString msg);
+    void receivedData(QString address,quint16 ip,QByteArray msg);
+};
 class control : public QObject
 {
     Q_OBJECT
@@ -15,12 +29,16 @@ public:
 private:
     wss * mywss=nullptr;
 
+    QHash<QString, userInfo *> _hashUserInfo;
 signals:
-
+    void signal_sendText(QString address,quint16 ip,QString msg);
+    void signal_sendData(QString address,quint16 ip,QByteArray msg);
 public slots:
-
+    void slot_connectTo(QString id,QString toID);
 private slots:
-    void slots_newU(QString ip,quint16 port,QUrl url);
+    void slot_receivedText(QString address,quint16 ip,QString msg);//处理文字信息 rpn?
+    void slot_receivedData(QString address,quint16 ip,QByteArray msg);//处理二进制 基本只转发
+    void slot_newU(QString ip,quint16 port,QUrl url);
 };
 
 #endif // CONTROL_H
